3194e10045eab78ade027d4598249fad9af7854b
Ian Connor <iconnor@projectlounge.com>
2018-06-23 13:41:22 +1000

    Remove synced files

--- index_files/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('.main-banner').height($(window).height());
-  $('.main-banner img').css('max-height', $(window).height()/2.2);
-});
-
-$(window).bind('load resize', function(){
-  $('.main-banner').height($(window).height());
-  $('.main-banner img').css('max-height', $(window).height()/2.2);
-});
